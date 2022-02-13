37fec1d2c77313619a9d741c5a3e61e69da33e0d
Thoriq Wajedi <thoriqwajedi88@gmail.com>
2022-02-13 12:25:04 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+function reveal() {
+    var reveals = document.querySelectorAll(".reveal");
+    for (var i = 0; i < reveals.length; i++) {
+        var windowHeight = window.innerHeight;
+        var elementTop = reveals[i].getBoundingClientRect().top;
+        var elementVisible = 100;
+        if (elementTop < windowHeight - elementVisible) {
+            reveals[i].classList.add("active");
+        } else {
+            reveals[i].classList.remove("active");
+        }
+    }
+}
+
+window.addEventListener("scroll", reveal);
+
+let searchBar = document.querySelector(".search-bar")
+let searchIcon = document.querySelector(".search-icon")
+
+searchIcon.addEventListener("click", function(){
+    searchBar.classList.toggle("active");
+    console.log(searchBar);
+})
